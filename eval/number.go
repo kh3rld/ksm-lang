23caf7d29dad37af731d89c88793f9ebbdcf3fd8
khahussein <kherld.hussein@gmail.com>
2024-09-03 20:33:43 +0300

    Feat(number): Define Number Type and Methods in number.go. - Added a  type to represent numerical values in Kisumu. - Implemented methods for arithmetic operations: addition and subtraction. - Added methods for type conversion between integer and float. - Provided methods for retrieving the string representation and numeric value.

---
+++ eval/number.go
@@ -0,0 +1,27 @@
+package eval
+
+import "fmt"
+
+type Number struct {
+	Value float64
+}
+
+func (n *Number) Add(other *Number) *Number {
+	return &Number{Value: n.Value + other.Value}
+}
+
+func (n *Number) Subtract(other *Number) *Number {
+	return &Number{Value: n.Value - other.Value}
+}
+
+func (n *Number) ToInt() int {
+	return int(n.Value)
+}
+
+func (n *Number) ToFloat() float64 {
+	return n.Value
+}
+
+func (n *Number) String() string {
+	return fmt.Sprintf("%f", n.Value)
+}
