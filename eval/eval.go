98318d83785f63f7f18ff1c72b6770aa57490324
khahussein <kherld.hussein@gmail.com>
2024-09-03 20:36:31 +0300

    feat(eval): Implement Evaluation Logic in eval.go - Added evaluation logic for the  type in the Kisumu interpreter. - Implemented methods to evaluate arithmetic expressions involving numbers. - Added handling for number type operations in the evaluation phase.

--- eval/eval.go
+++ eval/eval.go
@@ -9,3 +9,21 @@ type Evaluator struct{}
 func (e *Evaluator) VisitNumberExpr(expr *parser.NumberExpr) interface{} {
 	return &Number{Value: expr.Value}
 }
+
+func Eval(node parser.Expression) *Number {
+	switch n := node.(type) {
+	case *parser.NumberExpr:
+		return &Number{Value: n.Value}
+	}
+	return nil
+}
+
+func EvaluateArithmetic(left, right *Number, op string) *Number {
+	switch op {
+	case "+":
+		return left.Add(right)
+	case "-":
+		return left.Subtract(right)
+	}
+	return nil
+}
