069d21ce65354b76fdab58731685fe23d84957d7
doonyango <datieno001@gmail.com>
2024-09-04 20:22:21 +0300

    feat:implement parse statement add correct path and add value to the struct

--- parser/parser.go
+++ parser/parser.go
@@ -1,15 +1,17 @@
 package parser
 
 import (
-	"go/token"
+	"strconv"
 
 	"github.com/kh3rld/ksm-lang/lexer"
+	"github.com/kh3rld/ksm-lang/token"
 )
 
 type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	Value     int
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -21,20 +23,36 @@ func New(l *lexer.Lexer) *Parser {
 
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
-	// p.peekToken = p.l.NextToken()
+	p.peekToken = p.curToken
 }
 
-func (p *Parser) ParseProgram() Program {
-	var program Program
+func (p *Parser) ParseProgram() *Program {
+	program := &Program{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
 	return program
 }
 
-func (p *Parser) parseNumber() {
-	// literal := p.curToken.Literal
-	// value, err := strconv.ParseFloat(literal, 64)
-	// if err != nil {
-	// 	// Handle error appropriately
-	// 	return nil
-	// }
-	// return &NumberExpr{Value: value}
+func (p *Parser) parseStatement() Statement {
+	switch p.curToken.Type {
+	case token.NUMBER:
+		return p.parseNumber()
+	default:
+		return nil
+	}
 }
+
+func (p *Parser) parseNumber() *Parser {
+	value, err := strconv.Atoi(p.curToken.Literal)
+	if err != nil {
+		return nil
+	}
+	return &Parser{Value: value}
+}
+
