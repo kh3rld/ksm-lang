6f0a78fcb3dc81b67723faba79599b916ffd4ec9
doonyango <datieno001@gmail.com>
2024-09-14 14:20:54 +0300

    fix: remove redundancy

--- parser/ast.go
+++ parser/ast.go
@@ -30,37 +30,3 @@ type BinaryExpr struct {
 func (b *BinaryExpr) Accept(visitor Visitor) interface{} {
 	return visitor.VisitBinaryExpr(b)
 }
-
-package parser
-
-type Visitor interface {
-	VisitNumberExpr(expr *NumberExpr) interface{}
-	VisitBinaryExpr(expr *BinaryExpr) interface{}
-}
-
-type Node interface {
-	Accept(visitor Visitor) interface{}
-}
-
-type Program struct {
-	Statements []Node
-}
-
-type NumberExpr struct {
-	Value float64
-}
-
-func (n *NumberExpr) Accept(visitor Visitor) interface{} {
-	return visitor.VisitNumberExpr(n)
-}
-
-type BinaryExpr struct {
-	Left     Node
-	Operator string
-	Right    Node
-}
-
-func (b *BinaryExpr) Accept(visitor Visitor) interface{} {
-	return visitor.VisitBinaryExpr(b)
-}
-
