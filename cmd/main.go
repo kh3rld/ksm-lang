39446cb56032c759227dd12d893ac4c5b9bbfa7b
doonyango <datieno001@gmail.com>
2024-09-22 15:04:45 +0300

    refactor: remove unused code

--- cmd/main.go
+++ cmd/main.go
@@ -1,55 +1,34 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"os"
-
-// 	"github.com/kh3rld/ksm-lang/lexer"
-// 	t "github.com/kh3rld/ksm-lang/token"
-// )
-
-// func main() {
-// 	if len(os.Args) < 2 {
-// 		fmt.Println("Usage: ksm <file.ksm>")
-// 		os.Exit(1)
-// 	}
-
-// 	// Read source file
-// 	sourceFile := os.Args[1]
-// 	input, err := os.ReadFile(sourceFile)
-// 	if err != nil {
-// 		log.Fatalf("Error reading file: %v", err)
-// 	}
-
-// 	l := lexer.New(string(input))
-
-// 	for token := l.NextToken(); token.Type != t.EOF; token = l.NextToken() {
-// 		fmt.Printf("%+v\n", token)
-// 	}
-// }
-
 package main
 
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/kh3rld/ksm-lang/lexer"
 	"github.com/kh3rld/ksm-lang/parser"
+	t "github.com/kh3rld/ksm-lang/token"
 )
 
 func main() {
-	inputs := []string{
-		"42",
-		"-42",
-		"3.14",
-		"-3.14",
-		"not_a_number",
-		"0",
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ksm <file.ksm>")
+		os.Exit(1)
 	}
 
-	for _, input := range inputs {
+	// Read source file
+	sourceFile := os.Args[1]
+	inputs, err := os.ReadFile(sourceFile)
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+
+	l := lexer.New(string(inputs))
+
+	for token := l.NextToken(); token.Type != t.EOF; token = l.NextToken() {
+		fmt.Printf("%+v\n", token)
+	}
+	for _, input := range []string{string(inputs)} {
 		l := lexer.New(input)
 		p := parser.New(l)
 
@@ -57,7 +36,7 @@ func main() {
 
 		if numberExpr == nil {
 			if len(p.Errors()) > 0 {
-				log.Printf("Error parsing input '%s': %v\n", input, p.Errors())
+				log.Printf("Error parsing input %s: %v", input, p.Errors())
 			} else {
 				log.Printf("Unexpected error for input '%s': nil returned but no errors recorded.\n", input)
 			}
