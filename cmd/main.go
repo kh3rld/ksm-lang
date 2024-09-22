41514325c0b12bec980fd4a19b643806b568a6d1
Doreen-Onyango <167855655+Doreen-Onyango@users.noreply.github.com>
2024-09-22 13:39:57 +0300

    Bool (#47)
    * feat: implement main for number parser
    
    * resfactor: add implementation for negative numbers
    
    * test: add parser tests

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,68 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"github.com/kh3rld/ksm-lang/lexer"
+// 	t "github.com/kh3rld/ksm-lang/token"
+// )
+
+// func main() {
+// 	if len(os.Args) < 2 {
+// 		fmt.Println("Usage: ksm <file.ksm>")
+// 		os.Exit(1)
+// 	}
+
+// 	// Read source file
+// 	sourceFile := os.Args[1]
+// 	input, err := os.ReadFile(sourceFile)
+// 	if err != nil {
+// 		log.Fatalf("Error reading file: %v", err)
+// 	}
+
+// 	l := lexer.New(string(input))
+
+// 	for token := l.NextToken(); token.Type != t.EOF; token = l.NextToken() {
+// 		fmt.Printf("%+v\n", token)
+// 	}
+// }
+
 package main
 
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/kh3rld/ksm-lang/lexer"
-	t "github.com/kh3rld/ksm-lang/token"
+	"github.com/kh3rld/ksm-lang/parser"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: ksm <file.ksm>")
-		os.Exit(1)
+	inputs := []string{
+		"42",
+		"-42",
+		"3.14",
+		"-3.14",
+		"not_a_number",
+		"0",
 	}
 
-	// Read source file
-	sourceFile := os.Args[1]
-	input, err := os.ReadFile(sourceFile)
-	if err != nil {
-		log.Fatalf("Error reading file: %v", err)
-	}
+	for _, input := range inputs {
+		l := lexer.New(input)
+		p := parser.New(l)
 
-	l := lexer.New(string(input))
+		numberExpr := p.ParseNumber()
 
-	for token := l.NextToken(); token.Type != t.EOF; token = l.NextToken() {
-		fmt.Printf("%+v\n", token)
+		if numberExpr == nil {
+			if len(p.Errors()) > 0 {
+				log.Printf("Error parsing input '%s': %v\n", input, p.Errors())
+			} else {
+				log.Printf("Unexpected error for input '%s': nil returned but no errors recorded.\n", input)
+			}
+		} else {
+			fmt.Printf(" %v\n", numberExpr.Value)
+		}
 	}
 }
