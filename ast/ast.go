561c2cf97f99e3da07afdbf9312aeece85d8c12a
khahussein <kherld.hussein@gmail.com>
2024-09-22 14:37:46 +0300

    Implement AST nodes for expressions and statements
    - Introduced interfaces for Node, Statement, and Expression.
    - Added Program, Num, Identifier, InfixExpression, and ExpressionStatement types.
    - Implemented TokenLiteral and String methods for each type to support AST functionality.
    - Enhanced structure for representing and manipulating expressions and statements.

---
+++ ast/ast.go
@@ -0,0 +1,98 @@
+package ast
+
+import (
+	"bytes"
+
+	"github.com/kh3rld/ksm-lang/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+	String() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+type Num struct {
+	Value float64
+	Token token.Token
+}
+
+func (n *Num) expressionNode()      {}
+func (n *Num) TokenLiteral() string { return n.Token.Literal }
+func (n *Num) String() string       { return n.Token.Literal }
+
+// Expressions
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
+
+type InfixExpression struct {
+	Token    token.Token // The operator token, e.g. +
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (oe *InfixExpression) expressionNode()      {}
+func (oe *InfixExpression) TokenLiteral() string { return oe.Token.Literal }
+func (oe *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(oe.Left.String())
+	out.WriteString(" " + oe.Operator + " ")
+	out.WriteString(oe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
